c09ce2a6b05f0872750da7de15e1b9bb47951d55
lyc-1995 <liyuchen_1995@outlook.com>
2022-03-22 15:32:23 +0800

    Re-write documents for 'color_list'

--- R/color_list.R
+++ R/color_list.R
@@ -3,21 +3,5 @@
 #' The built-in list of color sets used for different plot types.
 #'
 #' @format A list with the following preset palettes
-#' \describe{
-#'   \item{venn}
-#'   \item{line}
-#'   \item{pie}
-#'   \item{twopie}
-#'   \item{bar}
-#'   \item{hist}
-#'   \item{heatmap}
-#'   \item{dot}
-#'   \item{box}
-#'   \item{all}
-#'   \item{gray}
-#'   \item{onlinereport}
-#'   \item{tsne}
-#'   \item{ggsci}
-#' }
 #'
 "color_list"
